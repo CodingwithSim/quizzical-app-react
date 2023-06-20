72724b9a5134842979ca924a5baf63061fc90bf8
CodingwithSim <113305177+CodingwithSim@users.noreply.github.com>
2023-06-20 17:51:48 +0100

    Initial commit

---
+++ src/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Menu(props) {
+  return (
+    <div className='menu'>
+      <h1 className='menu-title'> Quizzical</h1>
+      <span className='menu-description'>Description</span>
+      <button className='start-btn' onClick={() => props.start()}>Start</button>
+    </div>
+  )
+}
+
+export default Menu
